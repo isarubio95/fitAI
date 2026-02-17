47187269b7d19618d4e081486becc37a9e862ddd
isarubio95 <isarubio95@gmail.com>
2026-02-17 12:32:24 +0100

    update cta "Iniciar Entrenamiento"

--- src/components/routine/SortableRoutineCard.tsx
+++ src/components/routine/SortableRoutineCard.tsx
@@ -143,7 +143,7 @@ export function SortableRoutineCard({
           </div>
         </div>
 
-        <Button className="w-full mt-3" onClick={() => onStart(r)}>
+        <Button className="w-full mt-3 bg-green-700" onClick={() => onStart(r)}>
           <Play className="h-4 w-4 mr-2" /> Iniciar Entrenamiento
         </Button>
       </CardContent>
